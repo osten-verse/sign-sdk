e67963dd9bb977a127de8cc21068242505061646
andre.marques <andre.marques@ostenmoove.com.br>
2024-09-23 09:05:57 -0300

    fix/envelope module has been commented

--- src/modules/Envelopes.ts
+++ src/modules/Envelopes.ts
@@ -1,38 +1,38 @@
-import { Routers } from '../services/RoutersService'
-import { Signers } from './EnvelopeSigner'
+// import { Routers } from '../services/RoutersService'
+// import { Signers } from './EnvelopeSigner'
 
-export class Envelope {
-  private routers: Routers
-  private envelope: any
+// export class Envelope {
+//   private routers: Routers
+//   private envelope: any
 
-  private constructor(routers: Routers, envelope?: any) {
-    this.routers = routers
-    this.envelope = envelope
-  }
+//   private constructor(routers: Routers, envelope?: any) {
+//     this.routers = routers
+//     this.envelope = envelope
+//   }
 
-  static start(routers: Routers) {
-    return new Envelope(routers)
-  }
+//   static start(routers: Routers) {
+//     return new Envelope(routers)
+//   }
 
-  static startByRequest(routers: Routers, envelope: any) {
-    return new Envelope(routers, envelope)
-  }
+//   static startByRequest(routers: Routers, envelope: any) {
+//     return new Envelope(routers, envelope)
+//   }
 
-  print() {
-    console.log(this.envelope)
-  }
+//   print() {
+//     console.log(this.envelope)
+//   }
 
-  close() {}
+//   close() {}
 
-  signer(): Signers {
-    if (!this.envelope.signer) this.envelope.signer = new Signers(this.routers)
+//   signer(): Signers {
+//     if (!this.envelope.signer) this.envelope.signer = new Signers(this.routers)
 
-    return this.envelope.signers
-  }
-}
+//     return this.envelope.signers
+//   }
+// }
 
-const router = new Routers('', '', '')
-const envelope = Envelope.start(router)
-const signers = envelope.signer().list()
-signers.map((it) => envelope.signer().get(it.id).remove())
-envelope.close()
+// const router = new Routers('', '', '')
+// const envelope = Envelope.start(router)
+// const signers = envelope.signer().list()
+// signers.map((it) => envelope.signer().get(it.id).remove())
+// envelope.close()
